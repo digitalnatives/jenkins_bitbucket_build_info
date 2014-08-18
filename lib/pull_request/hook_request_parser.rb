d74799d56140d7c45808b0b094013a75f64f6cd5
Iuri Fernandes <iuri.fernandes@digitalnatives.hu>
2014-08-18 13:27:23 +0200

    Parse comment hook to support restarting.

--- lib/pull_request/hook_request_parser.rb
+++ lib/pull_request/hook_request_parser.rb
@@ -10,7 +10,7 @@ class PullRequest::HookRequestParser
   end
 
   def can_trigger_a_build?
-    [:created, :updated].include?(hook_type)
+    [:created, :updated].include?(hook_type) || restart_comment?
   end
 
   # The next methods won't work for all cases. The cases that they
@@ -24,15 +24,33 @@ class PullRequest::HookRequestParser
   end
 
   def destination
-    body.destination.repository.full_name
+    if restart_comment?
+      body.links.self.href.split(/(repositories|pullrequests)/)[2][1..-2]
+    else
+      body.destination.repository.full_name
+    end
   end
 
   def sha
-    body.source.commit[:hash][0..11]
+    full_sha = if restart_comment?
+                 sha_match = body.content.raw.match(/\[ci restart (.*)\]/)
+                 sha_match && sha_match[1]
+               else
+                 body.source.commit[:hash]
+               end
+    full_sha && full_sha[0..11]
   end
 
   def attributes_hash
-    { user: username, sha: sha, repo: repository }
+    { user: username, sha: sha, repo: repository, restart: restart_comment? }
+  end
+
+  def restart_comment?
+    if @restart_comment.nil?
+      @restart_comment = (hook_type == :comment_created &&
+                          body.content.raw.include?("[ci restart"))
+    end
+    @restart_comment
   end
 
   private
